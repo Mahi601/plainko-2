b2bb537140fa6210dd9d1a1536948445c09844da
Mahi <77668368+Mahi601@users.noreply.github.com>
2021-05-10 17:18:49 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,16 @@
+class particle{
+    constructor(x,y){
+       
+this.body=b.circle(x,y,10)    
+w.add(wo,this.body)    
+    }
+display(){
+    ellipseMode(RADIUS)
+    fill (random (0,255),random(0,255),random(0,255))
+ellipse(this.body.position.x,this.body.position.y,10,10)
+}
+
+
+
+
+}
